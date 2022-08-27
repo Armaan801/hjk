b6bcd9b1a653c615e41d6e72f362a36a5bdc4322
Armaan801 <88334584+Armaan801@users.noreply.github.com>
2022-08-27 12:15:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup()
+{
+    cnavas = createCnavas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
